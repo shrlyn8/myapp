ca7e625512171513f5b4fcc5d50eaee001edab4d
shrlyn <shrlyncatid@gmail.com>
2026-04-18 19:47:19 +0800

    fix routes properly

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\DB; // 👈 ADD THIS
 use App\Http\Controllers\TodoController;
 
-Route::get('/todos', [TodoController::class, 'index']);
-Route::get('/todos/create', [TodoController::class, 'create']);
-Route::post('/todos', [TodoController::class, 'store']);
-Route::get('/todos/{id}/edit', [TodoController::class, 'edit']);
-Route::post('/todos/{id}/update', [TodoController::class, 'update']);
-Route::get('/todos/{id}/delete', [TodoController::class, 'destroy']);
+Route::redirect('/', '/todos');
 
-Route::get('/about', function () {
-    return view('about');
-});
-
-Route::get('/test-db', function () {
-    return DB::connection()->getPdo();
-});
-
-Route::get('/', function () {
-    return "Laravel Railway Working 🚀";
-});
+Route::resource('todos', TodoController::class)->except(['show']);
